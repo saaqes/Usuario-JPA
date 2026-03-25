6845131787e7daf46d728f11d6d00a5477c96141
saaqes <jramirezceballos27@gmail.com>
2026-03-25 17:11:14 -0500

    Update UsuarioRepository.java

--- src/main/java/com/JPA/demo/repository/UsuarioRepository.java
+++ src/main/java/com/JPA/demo/repository/UsuarioRepository.java
@@ -1,10 +1,15 @@
 package com.JPA.demo.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.JPA.demo.entity.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
-    Usuario findByCorreo(String correo);
+    // 🔹 Buscar usuario por correo
+    Optional<Usuario> buscarPorCorreo(String correo);
+
 }
