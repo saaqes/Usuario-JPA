d8bb52878ae3ff7ecf2a375e420f9a36e36410cb
saaqes <jramirezceballos27@gmail.com>
2026-03-25 17:09:24 -0500

    Update UsuarioController.java

--- src/main/java/com/JPA/demo/controller/UsuarioController.java
+++ src/main/java/com/JPA/demo/controller/UsuarioController.java
@@ -1,53 +1,45 @@
 package com.JPA.demo.controller;
 
-import java.util.List;
-
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.JPA.demo.entity.Usuario;
 import com.JPA.demo.service.UsuarioService;
 
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/usuarios")
-
 public class UsuarioController {
 
-    private final UsuarioService service;
+    private UsuarioService usuarioService;
 
-    public UsuarioController(UsuarioService service) {
-        this.service = service;
-    }
-
-    @PostMapping
-    public Usuario crear(@RequestBody Usuario usuario) {
-        return service.guardar(usuario);
+    public UsuarioController(UsuarioService usuarioService) {
+        this.usuarioService = usuarioService;
     }
 
     @GetMapping
-    public List<Usuario> listar() {
-        return service.listar();
+    public List<Usuario> obtenerTodos() {
+        return usuarioService.listar();
     }
 
     @GetMapping("/{id}")
-    public Usuario obtener(@PathVariable Long id) {
-        return service.obtenerPorId(id);
+    public Usuario buscarPorId(@PathVariable("id") Long id) {
+        return usuarioService.obtenerPorId(id);
+    }
+
+    @PostMapping
+    public Usuario guardarUsuario(@RequestBody Usuario nuevoUsuario) {
+        return usuarioService.guardar(nuevoUsuario);
     }
 
     @PutMapping("/{id}")
-    public Usuario actualizar(@PathVariable Long id, @RequestBody Usuario usuario) {
-        return service.actualizar(id, usuario);
+    public Usuario editarUsuario(@PathVariable Long id, @RequestBody Usuario datosActualizados) {
+        return usuarioService.actualizar(id, datosActualizados);
     }
 
     @DeleteMapping("/{id}")
-    public void eliminar(@PathVariable Long id) {
-        service.eliminar(id);
+    public String borrarUsuario(@PathVariable Long id) {
+        usuarioService.eliminar(id);
+        return "Usuario eliminado";
     }
 }
